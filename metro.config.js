a6032905047e36635cc213fc533704f21243669e
SandraLum <lumweiling@gmail.com>
2023-10-09 17:00:15 +0800

    Upgrade version and yarn

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config')
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname)
+
+config.watcher.additionalExts.push('mjs', 'cjs')
+
+module.exports = config
